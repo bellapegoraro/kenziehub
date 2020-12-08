e6ef55ee636b23726c759916ef0653b9a02dc224
Franciele <Franciele Cristine>
2020-12-08 17:23:13 -0300

    chore: layout buttons and top bar.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,28 @@
 import {useHistory} from 'react-router-dom';
+import {
+    Container,
+    TopBar,
+    Label,
+    Button,
+    Footer,
+    Content
+} from './style';
 
 const Home = () =>{
     const history = useHistory();
     return(
-        <div>
-            <div><h1>Kenzie Hub</h1></div>
-            <label>Já tenho conta</label>
-            <button onClick={history.push('/login')}>Login</button>
-            <label>Não tenho conta</label>
-            <button onClick={history.push('/cadastro')}>Cadastre-se</button>
-            <label>Entrar sem fazer login</label>
-            <button onClick={history.push('/devs')}>Devs</button>
-            <footer>Todos os direitos reservados</footer>
-        </div>
+        <Container>
+            <TopBar><h1>Kenzie <br/> Hub</h1></TopBar>
+            <Content>
+                <Label>Já tenho conta</Label>
+                <Button onClick={() => history.push('/login')}>Login</Button>
+                <Label>Não tenho conta</Label>
+                <Button onClick={() => history.push('/cadastro')}>Cadastre-se</Button>
+                <Label>Entrar sem fazer login</Label>
+                <Button onClick={() => history.push('/devs')}>Devs</Button>
+                <Footer>Todos os direitos reservados</Footer>
+            </Content>  
+        </Container>
     )
 }
 
