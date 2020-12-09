2db1f14480c67a1d17fffb2a3f1d2a0e15e88f9c
Viktor Fagionato <viktormfagionato@gmail.com>
2020-12-09 13:41:50 -0300

    fix: Removed the Link tag for the registration page.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { GlobalStyle } from "./styles";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Register from "./pages/Register/index";
-import Home from './pages/HomeScreen';
+import Home from "./pages/HomeScreen";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Link to="/register">Cadastrar</Link>
       <Switch>
         <Route exact path="/register">
           <Register></Register>
         </Route>
-        <Route path = "/">
-          <Home/>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </>
