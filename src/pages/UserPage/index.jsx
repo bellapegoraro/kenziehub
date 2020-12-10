18b4121945e2dbc46c0858d993fced10dcba60b1
Franciele <Franciele Cristine>
2020-12-10 09:47:01 -0300

    chore: columns with flexbox.

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,8 +1,23 @@
+import {
+    Col1,
+    Col2,
+    Col3,
+    Container
+} from './style';
+
 const UserProfile = () =>{
     return(
-        <div>
-            Perfil
-        </div>
+        <Container>
+            <Col1>
+                <h3>Ana Maria</h3>
+            </Col1>
+            <Col2>
+                <h4>Trabalhos</h4>
+            </Col2>
+            <Col3>
+                <h4>Dados pessoais</h4>
+            </Col3>
+        </Container>
     )
 }
 
---
+++ src/pages/UserPage/style/index.js
@@ -0,0 +1,24 @@
+import Styled from 'styled-components';
+
+const Container = Styled.div `
+    display: flex;
+    justify-content: space-between;
+    width: 90vw;
+`
+
+const Col1 = Styled.div `
+
+`
+const Col2 = Styled.div `
+
+`
+const Col3 = Styled.div `
+
+`
+
+export {
+    Col1,
+    Col2,
+    Col3,
+    Container,
+}
