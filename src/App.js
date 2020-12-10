4043079127acf88cd763746b750a05c483021d12
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2020-12-10 10:11:16 -0300

    feat: adding page for devs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { GlobalStyle } from "./styles";
 import { Switch, Route } from "react-router-dom";
 import Register from "./pages/Register/index";
 import Home from "./pages/HomeScreen";
+import Users from "./pages/UserList";
 
 function App() {
   return (
@@ -11,8 +12,11 @@ function App() {
         <Route path="/register">
           <Register></Register>
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/devs">
+          <Users />
         </Route>
       </Switch>
     </>
---
+++ src/pages/UserList/index.jsx
@@ -0,0 +1,11 @@
+import List from "../../components/List";
+
+const Users = () => {
+  return (
+    <>
+      <List />
+    </>
+  );
+};
+
+export default Users;
