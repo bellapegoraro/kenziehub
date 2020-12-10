5f3cdf2daf3e103ba4f64592a5162a87677fb4cc
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2020-12-10 18:39:08 -0300

    feat: creating folders for search component

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+// import { useSelector } from "react-redux";
+
+// const Search = () => {
+//   return (
+//     <div>
+//       <input name="name" onChange={() => handleInput} />
+//       <button onClick={handleSeach}>Pesquisar</button>
+//     </div>
+//   );
+// };
