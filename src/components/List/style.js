062272c71e1cf3021f94c37990226adbcbcf7294
Franciele <Franciele Cristine>
2020-12-10 15:45:30 -0300

    fixed: remove styled-component name.

--- src/components/List/style.js
+++ src/components/List/style.js
@@ -55,10 +55,6 @@ const Infos = styled.div`
   }
 `;
 
-const Name = styled.h3`
-  /* width: 40vw; */
-`;
-
 const Email = styled.div`
   margin-top: 3%;
   font-size: 0.75em;
@@ -94,4 +90,4 @@ const Image = styled.img`
   }
 `;
 
-export { Container, Image, Main, Techs, Name, Email, Infos };
+export { Container, Image, Main, Techs, Email, Infos };
