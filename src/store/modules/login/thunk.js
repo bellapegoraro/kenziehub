8289b266dd8873a3751cf8e7ecc993a6a04b9bf8
Viktor Fagionato <viktormfagionato@gmail.com>
2020-12-11 08:43:15 -0300

    update files

--- src/store/modules/login/thunk.js
+++ src/store/modules/login/thunk.js
@@ -6,16 +6,9 @@ export const loginThunk = (data, setAuthentication) => (dispach) => {
     .post(" https://kenziehub.me/sessions", { ...data })
 
     .then((res) => {
-<<<<<<< HEAD
-      if (res.status !== "error") {
-        window.localStorage.setItem("authToken", res.data.token);
-        dispach(signIn(res.data));
-      }
-=======
       window.localStorage.setItem("authToken", res.data.token);
       setAuthentication(true);
       dispach(signIn(res.data));
->>>>>>> develop
     })
     .catch((err) => console.log(err.response));
 };
