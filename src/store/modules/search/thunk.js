817d89cd74fecc063a36f4b5c8be79e9935a2c50
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2020-12-14 10:28:23 -0300

    fix: search working

--- src/store/modules/search/thunk.js
+++ src/store/modules/search/thunk.js
@@ -1,7 +1,18 @@
 import { searchUser } from "./actions";
+import api from "../../../services/api";
 
 export const searchUserThunk = (input) => (dispatch, getState) => {
-  const { users } = getState();
-  const userFiltered = users.filter((user) => user.name === input);
-  dispatch(searchUser(userFiltered));
+  let users = [];
+  api
+    .get("/users", { params: { perPage: 500, page: 1 } })
+    .then((res) => {
+      users = [...users, ...res.data];
+
+      const userFiltered = users.filter((user) =>
+        user.name.toLowerCase().includes(input.toLowerCase())
+      );
+
+      dispatch(searchUser(userFiltered));
+    })
+    .catch((err) => console.log(err));
 };
